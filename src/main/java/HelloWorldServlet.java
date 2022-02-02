05d277aa5740acf20b7fa1ecb2eed7ae49d7feed
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-02 12:18:53 -0600

    Completed problem 5, starting bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -10,17 +10,20 @@ import java.util.ArrayList;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+    private int counter;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
+        counter++;
 
         PrintWriter out = res.getWriter();
 //        out.println("<h1>Hello World!</h1>");
 
-//        Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
 //        String name = req.getParameter("name");
 //        out.println("<h1>Hello " + name + " !</h1>");
 
+        //Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
         String name = req.getParameter("name");
 
         if(name != null) {
@@ -28,5 +31,12 @@ public class HelloWorldServlet extends HttpServlet {
         }else {
             out.println("<h1>Hello World!</h1>");
         }
+
+        out.println("Number of page views: " + counter);
+
+//      Create a page that displays a number that goes up by one every time the /count page is viewed.
+//      BONUS: Allow the user to pass a parameter in the query string to reset the counter.
+
+
     }//end doGet
 }//end HelloWorldServlet
