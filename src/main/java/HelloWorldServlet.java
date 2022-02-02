6f1b6c0b9c15d2e75a908aec3a36a60a6f4f5c76
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-02 11:55:01 -0600

    Completed main portion of exercise, starting bonuses

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,23 @@
+
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
 
-    }
-}
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello World!</h1>");
+
+
+//        Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+    }//end doGet
+}//end HelloWorldServlet
