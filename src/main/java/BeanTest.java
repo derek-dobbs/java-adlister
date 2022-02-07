1df4ba44b1b2ad6736a83836af52a0b924458cb3
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-07 11:01:46 -0600

    Updated exercise

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class BeanTest {
+    public static void main(String[] args) {
+        Album album1 = new Album();
+        Album album2 = new Album();
+        Album album3 = new Album();
+
+        Author author1 = new Author();
+        Author author2 = new Author();
+        Author author3 = new Author();
+
+        Quote quote1 = new Quote();
+        Quote quote2 = new Quote();
+        Quote quote3 = new Quote();
+
+        ArrayList<Quote> quotesArray;
+
+
+    }
+}
