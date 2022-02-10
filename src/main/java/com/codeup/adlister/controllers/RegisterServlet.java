8ca4b8806333e45427a7620365c255fdefcb960f
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-02-10 14:03:40 -0600

    Updated exercise

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,14 +1,18 @@
 package com.codeup.adlister.controllers;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        // TODO: show the registration form
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        // TODONE: show the registration form
+        response.setContentType("text/html");
+        request.getRequestDispatcher("/WEB-INF/ads/register.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
